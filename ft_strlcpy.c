1771a5ef0c2d40a4b0dbfe544bc6cb333101baa3
mogi <msleitao@proton.me>
2026-03-10 17:45:50 +0000

    ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: mleitao <mleitao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/09 22:48:32 by mleitao           #+#    #+#             */
-/*   Updated: 2026/03/09 23:03:23 by mleitao          ###   ########.fr       */
+/*   Updated: 2026/03/10 17:43:41 by mleitao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,13 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t			i;
 	unsigned int	src_len;
 
-	i = 0;
 	src_len = ft_strlen(src);
 	if (size == 0)
 		return (src_len);
-	while (src[i] && i < size - 1)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
+	while (*src && size-- > 1)
+		*dst++ = *src++;
+	*dst = '\0';
 	return (src_len);
 }
