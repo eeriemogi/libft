604343be1d451620a032941b1929844dc6463cae
mogi <msleitao@proton.me>
2026-03-12 19:18:07 +0000

    added atoi

---
+++ ft_atoi.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mleitao <mleitao@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/12 18:20:18 by mleitao           #+#    #+#             */
+/*   Updated: 2026/03/12 19:15:01 by mleitao          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int	sign;
+	int	res;
+
+	sign = 1;
+	res = 0;
+	while (*nptr == ' ' || (*nptr >= 9 && *nptr <= 13))
+		nptr++;
+	if (*nptr == '-' || *nptr == '+')
+	{
+		if (*nptr == '-')
+			sign = -1;
+		nptr++;
+	}
+	while (*nptr && ft_isdigit(*nptr))
+		res = res * 10 + *nptr++ - '0';
+	return (sign * res);
+}
