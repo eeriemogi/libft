c43a12882a33bdda8f52683c2091d566044a686a
mogi <msleitao@proton.me>
2026-03-14 02:05:52 +0000

    changed split (smarter version)

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: mleitao <mleitao@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/13 18:34:00 by mleitao           #+#    #+#             */
-/*   Updated: 2026/03/14 00:04:35 by mleitao          ###   ########.fr       */
+/*   Updated: 2026/03/14 02:04:26 by mleitao          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ static size_t	count_words(const char *s, char c)
 	return (w);
 }
 
-static char	*next_word(const char *s, char c)
+static char	*start_word(const char *s, char c)
 {
 	while (*s && *s == c)
 		s++;
@@ -45,20 +45,22 @@ static char	*end_word(const char *s, char c)
 	return ((char *)s);
 }
 
-static void	split(char **r, char **p, char const *s, char c)
+static void	split(char **r, char const *s, char c)
 {
 	char	*ewrd;
 	char	*wrd;
-	size_t	wrdl;
+	char	**p;
+	size_t	wls;
 
+	p = r;
 	while (*s)
 	{
-		s = next_word(s, c);
+		s = start_word(s, c);
 		if (!*s)
 			break ;
 		ewrd = end_word(s, c);
-		wrdl = ewrd - s;
-		wrd = malloc(sizeof(char) * (wrdl + 1));
+		wls = ewrd - s + 1;
+		wrd = malloc(sizeof(char) * wls);
 		if (!wrd)
 		{
 			while (p != r)
@@ -66,25 +68,21 @@ static void	split(char **r, char **p, char const *s, char c)
 			free(r);
 			return ;
 		}
-		while (s < ewrd)
-			*wrd++ = *s++;
-		*wrd = '\0';
-		*r++ = wrd - wrdl;
+		s += ft_strlcpy(wrd, s, wls) - ft_strlen(ewrd);
+		*p++ = wrd;
 	}
-	*r = NULL;
+	*p = NULL;
 }
 
 char	**ft_split(char const *s, char c)
 {
 	char	**r;
-	char	**p;
 
 	if (!s)
 		return (NULL);
 	r = malloc(sizeof(char *) * (count_words(s, c) + 1));
 	if (!r)
 		return (NULL);
-	p = r;
-	split(r, p, s, c);
+	split(r, s, c);
 	return (r);
 }
